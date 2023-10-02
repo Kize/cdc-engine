339268851743cd447018cf12b7fd0a5bc9376881
r1 <ecar@live.fr>
2023-10-02 23:48:18 +0200

    PlayerCard : random color

--- src/pages/scribe-panel/components/PlayerCard.tsx
+++ src/pages/scribe-panel/components/PlayerCard.tsx
@@ -40,10 +40,41 @@ export function PlayerCard({
 }): JSX.Element {
   const dispatch = useAppDispatch()
 
-  const card_detail = (
+  function random_hue(str) {
+    //https://www.30secondsofcode.org/js/s/hash-string-into-number/
+    let arr = str.split('');
+    let random_numb = arr.reduce(
+      (hashCode, currentVal) =>
+      (hashCode =
+        currentVal.charCodeAt(0) +
+        (hashCode << 6) +
+        (hashCode << 16) -
+        hashCode),
+      0
+    )
+    let random_hue = Math.abs(random_numb % 360)
+    return random_hue
+  }
+  const random_color = random_hue(details.player)
+
+  const card_detail = ((isCurrentPlayer) => (
     <Flex direction="row" alignItems="baseline">
-      <Box as="span" fontSize="3xl" fontWeight="light">{details.score}</Box>
-      <Box as="span" pt={2.5} fontWeight="light" verticalAlign="bottom">&nbsp;pts</Box>
+      <Box
+        as="span"
+        fontSize="3xl" 
+        fontWeight="normal"
+        color={details.isCurrentPlayer ? "white" : "black"}
+      >
+        {details.score}
+      </Box>
+      <Box 
+        as="span" pt={2.5} 
+        fontWeight="light" 
+        verticalAlign="bottom"
+        color={details.isCurrentPlayer ? "white" : "black"}
+      >
+        &nbsp;pts
+      </Box>
       <Spacer />
       <Tag colorScheme="red" hidden={!details.hasGrelottine}>
         <TagLeftIcon as={FaRegBell} />
@@ -54,35 +85,66 @@ export function PlayerCard({
         <TagLabel>Civet</TagLabel>
       </Tag>
     </Flex>
-  )
+  ))
 
   return (
     <Menu>
       <MenuButton as={Box}>
         <Card
           variant="filled"
-          bgColor={details.isCurrentPlayer ? "blue.50" : "#F9EDC4"}
-          bgGradient={details.isCurrentPlayer ? "linear(#FFCC6A, #FFE4B8 25%, #9E8426)" : ""}
+          //bgColor={details.isCurrentPlayer ? "blue.50" : "#F9EDC4"}
+          //bgGradient={details.isCurrentPlayer ? "linear(#FFCC6A, #FFE4B8 25%, #9E8426)" : ""}
+          bgColor={"hsl(" + random_color + ", 60%, 90%)"}
+          bgGradient={details.isCurrentPlayer
+            /*
+            ? "linear(#FFCC6A, #FFE4B8 25%, #9E8426)"
+            /*/
+            ? "linear(" +
+            "hsl(" + random_color + ", 60%, 90%)," +
+            "hsl(" + random_color + ", 60%, 80%) 25%," +
+            "hsl(" + random_color + ", 60%, 30%)" +
+            ")"
+            /**/
+            : ""}
         //bgImage="../../../../public/abstract-gold-leather-textures-free-photo.jpg"
         >
+          {/*
           <CardHeader px={3} py={1}>
             {card_detail}
+            {details.player}
+          </CardHeader>
+          /**/}
+
+          <CardHeader px={3} py={1}>
+            <Text
+              width="100%"
+              pt={2}
+              fontSize={["xl", "2xl", "2xl"]}
+              fontWeight="light"
+              align="center"
+              lineHeight={1}
+            >
+              {details.player}
+            </Text>
           </CardHeader>
 
+          {/** /}
           <CardBody px={0} py={0}>
             <Text
               width="100%"
               fontSize={["xl", "2xl", "2xl"]}
               fontWeight="light"
               align="center"
-            lineHeight={1}
+              lineHeight={1}
             >
               {details.player}
+              {/** /}<br />{random_hue(details.player)}{/*//*/}
             </Text>
           </CardBody>
+          {/**/}
 
-          <CardFooter display="block" px={3} py={1} opacity={0.5} transform="rotate(180deg)">
-            {card_detail}
+          <CardFooter display="block" px={3} py={0}/*py={1} opacity={0.5} transform="rotate(180deg)*/>
+            {card_detail(details.isCurrentPlayer)}
           </CardFooter>
         </Card>
       </MenuButton>
