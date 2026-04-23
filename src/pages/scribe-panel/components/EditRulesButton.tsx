915b50d6e7c13ced31b09d2d66b399bc996cde4a
Blandine Jarysta <blandine.jarysta@gmail.com>
2026-04-23 09:59:03 +0200

    disable button if same state

--- src/pages/scribe-panel/components/EditRulesButton.tsx
+++ src/pages/scribe-panel/components/EditRulesButton.tsx
@@ -8,19 +8,33 @@ import {
 	Button,
 	useDisclosure,
 } from "@chakra-ui/react";
-import { type JSX, useRef, useState } from "react";
+import { type JSX, useEffect, useRef, useState } from "react";
 import { MdModeEdit } from "react-icons/md";
 import { updateGameRulesThunk } from "../../../store/current-game/current-game-lifecycle-thunks.ts";
 import { useAppDispatch, useAppSelector } from "../../../store/store.ts";
 import { RulesSelectionPanel } from "../../create-new-game/RulesSelectionPanel.tsx";
 import { selectRulesConfiguration } from "../../../store/current-game/current-game-selectors.ts";
+import { configureGameHandlerRules } from "../../../utils/game-handler-configuration.ts";
+import type { RulesConfiguration } from "../../../../lib/rule-runner/rule-runner-configuration.ts";
 
 export function EditRulesButton(): JSX.Element {
 	const { isOpen, onOpen, onClose } = useDisclosure();
-    const rulesConfiguration = useAppSelector(selectRulesConfiguration);
-    const [rulesFrom, setRulesFrom] = useState(rulesConfiguration);
+	const rulesConfiguration = useAppSelector(selectRulesConfiguration);
+	const [rulesFrom, setRulesFrom] = useState(rulesConfiguration);
 	const cancelRef = useRef(null);
 	const dispatch = useAppDispatch();
+
+	useEffect(() => {
+		if (isOpen) {
+			setRulesFrom(rulesConfiguration);
+		}
+	}, [isOpen, rulesConfiguration]);
+
+	configureGameHandlerRules(rulesConfiguration);
+
+	const isRulesDisabled = (
+		Object.keys(rulesConfiguration) as Array<keyof RulesConfiguration>
+	).every((key) => rulesFrom[key] === rulesConfiguration[key]);
 
 	return (
 		<>
@@ -44,7 +58,7 @@ export function EditRulesButton(): JSX.Element {
 						</AlertDialogHeader>
 
 						<AlertDialogBody>
-                            <RulesSelectionPanel rules={rulesFrom} setRules={setRulesFrom} />
+							<RulesSelectionPanel rules={rulesFrom} setRules={setRulesFrom} />
 						</AlertDialogBody>
 
 						<AlertDialogFooter>
@@ -52,7 +66,8 @@ export function EditRulesButton(): JSX.Element {
 								Revenir en jeu
 							</Button>
 
-                            <Button
+							<Button
+								isDisabled={isRulesDisabled}
 								colorScheme="green"
 								onClick={async () => {
 									await dispatch(updateGameRulesThunk(rulesFrom));
@@ -60,7 +75,7 @@ export function EditRulesButton(): JSX.Element {
 								}}
 								ml={3}
 							>
-								Enregistrer les modifications
+								Valider
 							</Button>
 						</AlertDialogFooter>
 					</AlertDialogContent>
